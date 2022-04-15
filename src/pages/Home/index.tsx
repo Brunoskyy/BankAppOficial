67605a365bd009154f860b3fe01e8b58df0d58c5
Brunoskyy <arturbrunoferreira@gmail.com>
2022-04-15 19:58:49 -0300

    Create dashboard table

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,18 @@
 import {
   Box,
   Center,
+  Checkbox,
   Flex,
   Heading,
   HStack,
   Icon,
+  Table,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import {
   FaArrowCircleDown,
@@ -69,6 +76,7 @@ export const Home = () => {
             w="350px"
             h="220px"
             color="gray.500"
+            bg="white"
             justify="center"
           >
             <Center borderRadius="full" height="14" w="14" bgColor="green.400">
@@ -88,6 +96,7 @@ export const Home = () => {
             w="350px"
             h="220px"
             color="gray.500"
+            bg="white"
             justify="center"
           >
             <Center borderRadius="full" height="14" w="14" bgColor="red.400">
@@ -99,6 +108,55 @@ export const Home = () => {
             <Heading color="blackAlpha.800">R$2400,00</Heading>
           </Flex>
         </HStack>
+        <Box p={8} flex="1" w="1130px" bg="white" borderRadius="lg" mt="10">
+          <Table colorScheme="blackAlpha">
+            <Thead>
+              <Tr>
+                <Th>Funcionario</Th>
+                <Th>Valor transferido</Th>
+                <Th>Data da operação</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Joãozim</Text>
+                    <Text fontSize="sm" color="gray.500">
+                      email@mail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>R$ 1400,00</Td>
+                <Td>14/04/2022</Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Joãozim</Text>
+                    <Text fontSize="sm" color="gray.500">
+                      email@mail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>R$ 1400,00</Td>
+                <Td>14/04/2022</Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Joãozim</Text>
+                    <Text fontSize="sm" color="gray.500">
+                      email@mail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>R$ 1400,00</Td>
+                <Td>14/04/2022</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
       </Flex>
     </Box>
   );
