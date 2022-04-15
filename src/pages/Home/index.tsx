402825f56e8e7928c86373587470b858b7d6a847
Brunoskyy <arturbrunoferreira@gmail.com>
2022-04-15 17:23:13 -0300

    Create temporary header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,28 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const Home = () => {
   return (
-    <>
-      <Heading>Here we GO!!!</Heading>
-    </>
+    <Box w="full" m="0 auto" bgColor="gray.200" height="100vh">
+      <Flex
+        justifyContent="space-between"
+        paddingX="6"
+        paddingY="8"
+        bgColor="white"
+      >
+        <Flex>
+          <Text
+            fontSize={["2xl", "3xl"]}
+            fontWeight="bold"
+            letterSpacing="tight"
+            w="64"
+          >
+            bank
+            <Text as="span" ml="1" color="purple.500">
+              .io
+            </Text>
+          </Text>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
