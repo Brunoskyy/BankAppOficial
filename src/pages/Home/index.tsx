b5ce12e0e9d47fbe99efb1fbbb35d41d15a56bc0
Brunoskyy <arturbrunoferreira@gmail.com>
2022-04-15 18:36:12 -0300

    Create financial data cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import {
+  FaArrowCircleDown,
+  FaArrowCircleUp,
+  FaMoneyBillWaveAlt,
+  FaWallet,
+} from "react-icons/fa";
 
 export const Home = () => {
   return (
-    <Box w="full" m="0 auto" bgColor="gray.200" height="100vh">
+    <Box w="full" m="0 auto" bgColor="gray.100" height="100vh">
       <Flex
+        as="header"
         justifyContent="space-between"
         paddingX="6"
         paddingY="8"
@@ -22,6 +37,68 @@ export const Home = () => {
             </Text>
           </Text>
         </Flex>
+      </Flex>
+      <Flex paddingY="8" paddingX={5} flexFlow="column" align="center">
+        <HStack spacing={10}>
+          <Flex
+            flexFlow="column"
+            p={5}
+            shadow="md"
+            borderRadius="lg"
+            borderWidth="1px"
+            w="350px"
+            h="220px"
+            color="gray.500"
+            bg="gray.800"
+            justify="center"
+          >
+            <Center borderRadius="full" height="14" w="14" bgColor="purple.800">
+              <Icon as={FaMoneyBillWaveAlt} boxSize="8" color="purple.500" />
+            </Center>
+            <Text mt="6" mb="2" fontSize="2xl">
+              Balanço total
+            </Text>
+            <Heading color="gray.200">R$2400,00</Heading>
+          </Flex>
+          <Flex
+            flexFlow="column"
+            p={5}
+            shadow="md"
+            borderRadius="lg"
+            borderWidth="1px"
+            w="350px"
+            h="220px"
+            color="gray.500"
+            justify="center"
+          >
+            <Center borderRadius="full" height="14" w="14" bgColor="green.400">
+              <Icon as={FaArrowCircleUp} boxSize="8" color="green.200" />
+            </Center>
+            <Text mt="6" mb="2" fontSize="2xl">
+              Entradas
+            </Text>
+            <Heading color="blackAlpha.800">R$2400,00</Heading>
+          </Flex>
+          <Flex
+            flexFlow="column"
+            p={5}
+            shadow="md"
+            borderRadius="lg"
+            borderWidth="1px"
+            w="350px"
+            h="220px"
+            color="gray.500"
+            justify="center"
+          >
+            <Center borderRadius="full" height="14" w="14" bgColor="red.400">
+              <Icon as={FaArrowCircleDown} boxSize="8" color="red.200" />
+            </Center>
+            <Text mt="6" mb="2" fontSize="2xl">
+              Saídas
+            </Text>
+            <Heading color="blackAlpha.800">R$2400,00</Heading>
+          </Flex>
+        </HStack>
       </Flex>
     </Box>
   );
