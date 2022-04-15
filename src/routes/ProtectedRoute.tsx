c3246d4e3662dfa63767f4d8a732953eaa0fd095
Brunoskyy <arturbrunoferreira@gmail.com>
2022-04-15 17:22:41 -0300

    Remove unecessary import

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,5 +1,4 @@
 import { Outlet, Navigate } from "react-router-dom";
-import Signin from "../pages/Login";
 
 export const ProtectedRoutes = () => {
   const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
